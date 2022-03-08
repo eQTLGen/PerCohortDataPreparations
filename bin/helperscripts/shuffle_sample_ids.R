239f67c984207a42d4356069b59be8cff0736bd0
Urmo Võsa <urmo.vosa@gmail.com>
2022-03-08 11:19:30 +0200

    Small fixes

--- bin/helperscripts/shuffle_sample_ids.R
+++ bin/helperscripts/shuffle_sample_ids.R
@@ -2,21 +2,25 @@ args <- commandArgs(trailingOnly = TRUE)
 
 library(data.table)
 
+setDTthreads(8)
+
 # do not set seed
 # set.seed(123)
 
 exp <- fread(args[1], header = TRUE)
 cov <- fread(args[2], header = TRUE)
 
+colnames(exp)[1] <- "ID"
+
 # Take the intersection of samples over two datasets
-exp <- exp[exp$ID %in% cov$ID, ]
-cov <- cov[cov$ID %in% exp$ID, ]
+exp <- exp[exp$ID %in% cov$SampleID, ]
+cov <- cov[cov$SampleID %in% exp$ID, ]
 # Order the datasets based on sample ID, so that link between phenotype and covariate sample IDs remains 
 # after shuffling
 exp <- exp[order(exp$ID), ]
-cov <- cov[order(cov$ID), ]
+cov <- cov[order(cov$SampleID), ]
 
-if(!all(exp$ID == cov$ID)){stop("Error: phenotype and covariate sample IDs are not aligned!")}
+if(!all(exp$ID == cov$SampleID)){stop("Error: phenotype and covariate sample IDs are not aligned!")}
 
 exp_suff <- exp
 
@@ -26,8 +30,8 @@ cov_suff_expPC <- cov[, c(1, 11:111), with = FALSE]
 cov_suff_genPC <- cov[, c(1, 2:11), with = FALSE]
 
 exp_suff$ID <- sample(exp_suff$ID, length(exp_suff$ID))
-cov_suff_expPC$ID <- exp_suff$ID
-cov_suff <- merge(cov_suff_genPC, cov_suff_expPC, by = "ID")
+cov_suff_expPC$SampleID <- exp_suff$ID
+cov_suff <- merge(cov_suff_genPC, cov_suff_expPC, by = "SampleID")
 
 fwrite(exp_suff, args[3], sep = "\t", row.names = FALSE)
 fwrite(cov_suff, args[4], sep = "\t", row.names = FALSE)
