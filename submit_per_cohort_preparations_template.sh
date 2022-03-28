8de3982c48d35bd912ea69aac221e8638ba6cc9b
Urmo Võsa <urmo.vosa@gmail.com>
2022-03-28 21:56:52 +0300

    Update nextflow version

--- submit_per_cohort_preparations_template.sh
+++ submit_per_cohort_preparations_template.sh
@@ -17,7 +17,7 @@ genotypes_hdf5=[Folder with genotype files in .hdf5 format]
 qc_data_folder=[Folder containing QCd data, inc. expression and covariates]
 output_path=../output
 
-NXF_VER=20.10.0 ${nextflow_path}/nextflow run PerCohortDataPreparations.nf \
+NXF_VER=21.10.6 ${nextflow_path}/nextflow run PerCohortDataPreparations.nf \
 --hdf5 ${genotypes_hdf5} \
 --qcdata ${qc_data_folder} \
 --outdir ${output_path} \
