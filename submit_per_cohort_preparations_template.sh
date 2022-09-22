68d1530138cffbce985d4ec6ab50a17df4a9c9b9
CAWarmerdam <robert_warmerdam@hotmail.com>
2022-09-22 07:03:00 +0000

    fixed cachedirs

--- submit_per_cohort_preparations_template.sh
+++ submit_per_cohort_preparations_template.sh
@@ -15,6 +15,12 @@ module load squashfs/4.4
 # If you follow the eQTLGen phase II cookbook and analysis folder structure,
 # some of the following paths are pre-filled.
 # https://github.com/eQTLGen/eQTLGen-phase-2-cookbook/wiki/eQTLGen-phase-II-cookbook
+
+# We set the following variables for nextflow to prevent writing to your home directory (and potentially filling it completely)
+# Feel free to change these as you wish.
+export SINGULARITY_CACHEDIR=../../singularitycache
+export NXF_HOME=../../nextflowcache
+
 nextflow_path=../../tools
 
 genotypes_hdf5=../../3_ConvertVcf2Hdf5/output # Folder with genotype files in .hdf5 format
